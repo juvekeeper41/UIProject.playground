f6da37ab078e5651db7812ca172533400e13a273
juvekeeper41 <juvekeeper41@gmail.com>
2016-09-21 08:14:47 -0600

    Created UI Methods for the background

---
+++ Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var DynamicView = UIView(frame: CGRectMake(100, 200, 100, 100))
+DynamicView.backgroundColor=UIColor.cyanColor()
+DynamicView.layer.cornerRadius=40
+DynamicView.layer.borderWidth=3
+var alpha: CGFloat;(0.5)
+var center: CGPoint;(1)
+var bounds: CGRect;(0,0)
+var frame: CGRect;(5)
+var transform: CGAffineTransform;(7.3)
+
+
+
+
+
+
+
+
+
+
+
+
+
